f0305a132a802d395637c7846653adfe44727e6c
Zach Hornung <Zachhornung@comcast.net>
2021-03-09 17:17:48 -0800

    robot is looking good

--- public/styles/js/zachRNN.js
+++ public/styles/js/zachRNN.js
@@ -4,12 +4,13 @@ let trainedNet ;
 let longest ;
 train(getTrainingData());
 
-console.log(trainedNet(encode(adjustSize('today was a good day'))));
+console.log(predictEmotion('today was a good day, didnt have to use my ak'));
 
 function train(data){
   net.train(processTrainingData(data), {
     log: true,
-    learningRate: 0.2
+    learningRate: 0.05,
+    iterations: 20000
   });
   trainedNet = net.toFunction();
 }
@@ -55,17 +56,47 @@ function getTrainingData(){
     {input: 'it as amazing', output: {positive: 1}},
     {input: 'it was decent', output: {positive: 1}},
     {input: 'it was the greatest', output: {positive: 1}},
-    {input: 'it was not too bad', output: {positive: 1}},
+    // {input: 'it was not too bad', output: {positive: 1}},
     {input: 'good', output: {positive: 1}},
     {input: 'decent', output: {positive: 1}},
-    {input: 'not bad', output: {positive: 1}},
-    {input: 'not too bad', output: {positive: 1}},
+    // {input: 'not bad', output: {positive: 1}},
+    // {input: 'not too bad', output: {positive: 1}},
     {input: 'pretty good', output: {positive: 1}},
     {input: 'great', output: {positive: 1}},
     {input: 'amazing', output: {positive: 1}},
     {input: 'quite fantastic', output: {positive: 1}},
     {input: 'fantastic', output: {positive: 1}},
     {input: 'superb', output: {positive: 1}},
+    {input: 'it was good', output: {positive: 1}},
+    {input: 'today was a good day, didnt have to use my ak', output: {positive: 1}},
+    {input: 'good day', output: {positive: 1}},
+    
+    {input: 'absolutely', output: {positive: 1}},
+    {input: 'beautiful', output: {positive: 1}},
+    {input: 'cheery', output: {positive: 1}},
+    {input: 'delightful', output: {positive: 1}},
+    {input: 'excellent', output: {positive: 1}},
+    {input: 'fabulous', output: {positive: 1}},
+    {input: 'gorgeous', output: {positive: 1}},
+    {input: 'heavenly', output: {positive: 1}},
+    {input: 'ideal', output: {positive: 1}},
+    {input: 'jovial', output: {positive: 1}},
+    {input: 'kind', output: {positive: 1}},
+    {input: 'lovely', output: {positive: 1}},
+    {input: 'marvelous', output: {positive: 1}},
+    {input: 'nice', output: {positive: 1}},
+    {input: 'optimistic', output: {positive: 1}},
+    {input: 'perfect', output: {positive: 1}},
+    {input: 'quality', output: {positive: 1}},
+    {input: 'remarkable', output: {positive: 1}},
+    {input: 'superb', output: {positive: 1}},
+    {input: 'terrific', output: {positive: 1}},
+    {input: 'unreal', output: {positive: 1}},
+    {input: 'vibrant', output: {positive: 1}},
+    {input: 'wondrous', output: {positive: 1}},
+    {input: 'yes', output: {positive: 1}},
+    {input: 'zeal', output: {positive: 1}},
+    // {input: '', output: {positive: 1}},
 
     
 
@@ -74,6 +105,10 @@ function getTrainingData(){
     {input: 'today was pretty lame', output: {negative: 1}},
     {input: 'i needed a boost today', output: {negative: 1}},
     {input: 'my day was bad', output: {negative: 1}},
+    {input: 'it as a bad day', output: {negative: 1}},
+    {input: 'it was a really bad day', output: {negative: 1}},
+    {input: 'it was a terribly bad day', output: {negative: 1}},
+    {input: 'it was a horrible day', output: {negative: 1}},
     {input: 'my day was a long one', output: {negative: 1}},
     {input: 'my day was boring', output: {negative: 1}},
     {input: 'my day was lame', output: {negative: 1}},
@@ -106,6 +141,32 @@ function getTrainingData(){
     {input: 'not great', output: {negative: 1}},
     {input: 'not amazing', output: {negative: 1}},
     {input: 'not good at all', output: {negative: 1}},
+    {input: 'bad day', output: {negative: 1}},
+    
+    {input: 'awful', output: {negative: 1}},
+    {input: 'boring', output: {negative: 1}},
+    {input: 'cry', output: {negative: 1}},
+    {input: 'dreadful', output: {negative: 1}},
+    {input: 'evil', output: {negative: 1}},
+    {input: 'foul', output: {negative: 1}},
+    {input: 'ghastly', output: {negative: 1}},
+    {input: 'horrible', output: {negative: 1}},
+    {input: 'imperfect', output: {negative: 1}},
+    {input: 'junky', output: {negative: 1}},
+    {input: 'lousy', output: {negative: 1}},
+    {input: 'misshapen', output: {negative: 1}},
+    {input: 'negative', output: {negative: 1}},
+    {input: 'offensive', output: {negative: 1}},
+    {input: 'poor', output: {negative: 1}},
+    {input: 'questionable', output: {negative: 1}},
+    {input: 'revolting', output: {negative: 1}},
+    {input: 'stressful', output: {negative: 1}},
+    {input: 'terrible', output: {negative: 1}},
+    {input: 'unpleasant', output: {negative: 1}},
+    {input: 'vile', output: {negative: 1}},
+    {input: 'worthless', output: {negative: 1}},
+    {input: 'yucky', output: {negative: 1}},
+    // {input: '', output: {negative: 1}},
   ];
   longest = trainingData.reduce((a, b) =>
     a.input.length > b.input.length ? a : b).input.length;
